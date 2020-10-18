390f17e5d31a6e72e7cc17d560e3f92747ca0f1f
Yath Seanghay <seanghay.dev@gmail.com>
2020-10-18 21:55:52 +0700

    add new loader

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 A sample app with LiveData, Epoxy & Navigation Component.
 
+ 
--- app/src/main/java/com/seanghay/flood/epoxy/LoaderModel.kt
+++ app/src/main/java/com/seanghay/flood/epoxy/LoaderModel.kt
@@ -1,9 +1,35 @@
 package com.seanghay.flood.epoxy
 
+import android.graphics.drawable.Drawable
 import android.widget.FrameLayout
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.EpoxyModelClass
 import com.seanghay.flood.R
+import com.seanghay.flood.databinding.ComponentLoaderBinding
 
 @EpoxyModelClass(layout = R.layout.component_loader)
-abstract class LoaderModel: EpoxyModel<FrameLayout>()
+abstract class LoaderModel : EpoxyModel<FrameLayout>() {
+
+    private lateinit var binding: ComponentLoaderBinding
+
+    override fun bind(view: FrameLayout) {
+        binding = ComponentLoaderBinding.bind(view)
+
+        val drawable = AnimatedVectorDrawableCompat.create(
+            view.context, R.drawable.avd_anim
+        )
+        drawable?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+            override fun onAnimationEnd(d: Drawable?) {
+                drawable.start()
+            }
+        })
+
+        drawable?.start()
+        binding.imageView.setImageDrawable(drawable)
+    }
+
+    override fun unbind(view: FrameLayout) {
+    }
+}
