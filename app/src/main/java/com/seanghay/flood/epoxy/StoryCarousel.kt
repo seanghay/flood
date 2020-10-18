a15318fe76a18065e3744b0f2a6f7b3ff3dc6be2
Yath Seanghay <seanghay.dev@gmail.com>
2020-10-18 11:35:56 +0700

    add editor

--- app/src/main/java/com/seanghay/flood/epoxy/StoryCarousel.kt
+++ app/src/main/java/com/seanghay/flood/epoxy/StoryCarousel.kt
@@ -3,14 +3,17 @@ package com.seanghay.flood.epoxy
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
+import android.widget.EdgeEffect
+import androidx.recyclerview.widget.RecyclerView
 import com.airbnb.epoxy.Carousel
 import com.airbnb.epoxy.ModelView
 import com.seanghay.flood.R
 
 @ModelView(
     defaultLayout = R.layout.component_carousel_story,
-    saveViewState = true)
-class StoryCarousel: Carousel {
+    saveViewState = true
+)
+class StoryCarousel : Carousel {
 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
@@ -23,5 +26,4 @@ class StoryCarousel: Carousel {
     init {
         setBackgroundColor(Color.WHITE)
     }
-
 }
